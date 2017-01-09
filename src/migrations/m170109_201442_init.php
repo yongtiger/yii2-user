e20e467150a0f805d61ea073dc3398d6bcb72594
tiger yang <tiger yang>
2017-01-09 16:37:20 +0800

    User Basic: Activate via Email

--- src/migrations/m170109_201442_init.php
+++ src/migrations/m170109_201442_init.php
@@ -12,7 +12,7 @@
 
 use yii\db\Migration;
 
-class m130524_201442_init extends Migration
+class m170109_201442_init extends Migration
 {
     public function up()
     {
@@ -28,6 +28,7 @@ class m130524_201442_init extends Migration
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
+            'activation_key' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
